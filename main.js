c82c1f24224ec216c296f550c1acdb7dd960c963
VEdant732 <VedantAjay377@gmail.com>
2025-01-06 20:43:25 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+//function 2
+
+Var
+
+
+function multiply(value1,value2){
+    var storage= value1*value2
+    document.getElementById("display-multiply").innerHTML ="product :"+ storage;
+console.log(storage);  
+}
+multiply( 1,9/5);
+
+function multiply(value1,value2){
+    var storage= value1*value2
+    document.getElementById("display-multiply").innerHTML ="product :"+ storage;
+console.log(storage);  
+}
+multiply( 1,5/9);
